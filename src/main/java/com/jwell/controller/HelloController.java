9002c54a1a716ccbf0ba04250e49a107dffa8541
yangbin <371820101@qq.com>
2020-12-01 16:45:00 +0800

    Idea_ssm

---
+++ src/main/java/com/jwell/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.jwell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Administrator
+ * @date 2020-12-01 09:19
+ */
+@Controller
+@RequestMapping("/sys")
+public class HelloController {
+
+        @RequestMapping(value = "/hello" ,produces = "text/json;charset=utf-8",method = RequestMethod.GET)
+        @ResponseBody
+        public String sayHello(){
+            System.out.println("你大爷的");
+            return "嘿嘿";
+        }
+
+
+}
