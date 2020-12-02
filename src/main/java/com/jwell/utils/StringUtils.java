a8e23590ff778b6f4cc71751494935e9533470d7
yangbin <371820101@qq.com>
2020-12-02 12:12:55 +0800

    新增string工具类

---
+++ src/main/java/com/jwell/utils/StringUtils.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 tu.cn All right reserved. This software is the
+ * confidential and proprietary information of tu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tu.cn
+ */
+package com.jwell.utils;
+
+/**
+ * @author yangbin
+ * @date 2020-12-02 11:39
+ */
+public class StringUtils {
+
+    public boolean strIsNull(String string) {
+        return string == null || string.length() == 0;
+    }
+
+}
